5166b84b346d7149869093f2430c915765d4e809
Angeline <angellineeee@hotmail.com>
2017-02-13 01:33:27 +0800

    Add RequiredWordsFilter test

--- src/test/java/sg/edu/nus/comp/cs3219/module/RequiredWordsFilterTest.java
+++ src/test/java/sg/edu/nus/comp/cs3219/module/RequiredWordsFilterTest.java
@@ -8,24 +8,30 @@ import java.util.Set;
 import org.junit.Before;
 import org.junit.Test;
 
+import sg.edu.nus.comp.cs3219.model.Line;
 import sg.edu.nus.comp.cs3219.model.LineStorage;
 
 public class RequiredWordsFilterTest {
 	LineStorage inputLineStorage;
 	LineStorage afterFilterLineStorage;
 	RequiredWordsFilter filter;
-
+	
 	@Before
 	public void setUp() {
 		// TODO
 		inputLineStorage = new LineStorage();
 		afterFilterLineStorage = new LineStorage();
+		afterFilterLineStorage.addLine("The Day after Tomorrow");
+		afterFilterLineStorage.addLine("Day after Tomorrow the");
+		afterFilterLineStorage.addLine("Tomorrow the Day after");
+		
 		filter = new RequiredWordsFilter(afterFilterLineStorage);
 		Set<String> requiredWords = new HashSet<>();
 		requiredWords.add("Day");
 		requiredWords.add("Tomorrow");
 		filter.setRequiredWords(requiredWords);
-		inputLineStorage.addObserver(filter);
+
+		inputLineStorage.addObserver(filter);		
 	}
 
 	@Test
@@ -33,6 +39,7 @@ public class RequiredWordsFilterTest {
 		// TODO
 		inputLineStorage.addLine("The Day after Tomorrow");
 
+		assertEquals(2, afterFilterLineStorage.size());
 		assertEquals("Day after Tomorrow the", afterFilterLineStorage.get(0).toString());
 		assertEquals("Tomorrow the Day after", afterFilterLineStorage.get(1).toString());
 	}
